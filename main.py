5847d97da1e469bac2eb5e4164dffea20dd6f332
keinerex <Yurkov22Vl@gmail.com>
2025-01-25 17:32:58 +0700

    animation logic

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ if __name__ == '__main__':
     size = width, height = 500, 500
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
-    fps = 15
+    fps = 20
     running = True
     player = Player()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill((0, 0, 0))
+            else:
+                player.action(event)
+        screen.fill((255, 255, 255))
         player.update()
         all_sprites.draw(screen)
         clock.tick(fps)
--- stuff.py
+++ stuff.py
@@ -15,13 +15,10 @@ def load_image(name):
     image = pygame.image.load(fullname)
     return image
 
-def load_level():
-    pass
-
 def save():
     pass
 
-def cut_sheet(rect, sheet, rows, cols):
+def cut_sheet(sheet, rows, cols):
     rect = pygame.Rect(0, 0, sheet.get_width() // cols, sheet.get_height() // rows)
     frames = []
     for y in range(rows):
@@ -30,22 +27,59 @@ def cut_sheet(rect, sheet, rows, cols):
     return frames
 
 
+class level:
+    def __init__(self):
+        self.bg = None
+        self.lvl_obstacles = None
+        self.load_level(0)
+
+
+    def load_level(self, lvl_id):
+        self.bg = load_image(f'sprites/levels/{lvl_id}_bg.png')
+        self.lvl_obstacles = load_image(f'sprites/levels/{lvl_id}_obs.png')
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(player_group, all_sprites)
         self.rect = pygame.Rect(0, 0, 75, 95)
-        self.frames = [cut_sheet(self.rect, load_image('sprites/player/running.png'), 1, 10),
-                       cut_sheet(self.rect, load_image('sprites/player/attack.png'), 1, 10),
-                       cut_sheet(self.rect, load_image('sprites/player/attack2.png'), 1, 10),
-                       cut_sheet(self.rect, load_image('sprites/player/fall.png'), 1, 9)]
+        img_run = load_image('sprites/player/running.png')
+        img_attack = load_image('sprites/player/attack.png')
+        img_fall = load_image('sprites/player/fall.png')
+        self.frames = [cut_sheet(self.rect, img_attack, 1, 10)[:1],
+                       cut_sheet(self.rect, img_run, 1, 10),
+                       cut_sheet(self.rect, img_attack, 1, 10),
+                       cut_sheet(self.rect, img_fall, 1, 9)]
         self.frame = 0
-        self.state = 1
+        self.state = 0
         self.image = self.frames[self.state][self.frame]
+
+    def action(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                self.state = 1
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                self.state = 0
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            self.frame = 0
+            if event.button == 1:
+                self.state = 2
 
 
     def update(self):
+        if self.state == 2:
+            self.frame = (self.frame + 1)
+            if self.frame >= len(self.frames[self.state]):
+                self.state = 0
+                self.frame = 0
+        elif self.state == 3:
+            frame = (self.frame + 1)
+            if frame < len(self.frames[self.state]):
+                self.frame = frame
+        else:
+            self.frame = (self.frame + 1) % len(self.frames[self.state])
         self.image = self.frames[self.state][self.frame]
-        self.frame = (self.frame + 1) % len(self.frames[self.state])
 
 
 class Enemy(pygame.sprite.Sprite):
